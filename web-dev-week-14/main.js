7a8d93a61f663daa21bc9a17e00e42be9e640b99
Karmpatel18 <karmpatel1203@gmail.com>
2025-05-10 19:47:16 +0530

    typescript week 14

---
+++ web-dev-week-14/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let integer = 14;
+console.log(integer);
+function greeting(name) {
+    console.log("hello " + name);
+}
+greeting("karm");
---
+++ web-dev-week-14/main.ts
@@ -0,0 +1,6 @@
+let integer: number =  14;
+console.log(integer);
+function greeting(name: string){
+    console.log("hello "+ name);
+}
+greeting("karm");
