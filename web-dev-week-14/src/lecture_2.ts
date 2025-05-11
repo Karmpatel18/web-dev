448619030cb3dba1c6d5b224c048ae53475081b9
Karmpatel18 <karmpatel1203@gmail.com>
2025-05-12 00:53:08 +0530

    interface in classes

--- web-dev-week-14/src/lecture_2.ts
+++ web-dev-week-14/src/lecture_2.ts
@@ -1,12 +1,18 @@
+interface Address {
+        city: string,                
+        country: string,    
+        pincode: number
+    
+}
+// interfaces can use other interfaces as well like below userInterface is using Address interface
 interface userInterface {
     name: string,
     age: number,
-    address: {
-        city: string,
-        country: string,
-        pincode: number
-    }
+    address?: Address        
 }
+//to make the address field optional 
+// user "?" before : "colon"  
+// like "address?: { code here }"
 
 let user:userInterface = {
     name: "karm",
@@ -24,5 +30,35 @@ function isLegal(user:userInterface):boolean{
     }
     else return false
 }
-const result = isLegal(user)
-console.log(result)
+// const result = isLegal(user)
+// console.log(result)
+
+interface People {
+    name: string,
+    age: number,
+    greet?: ()  => string,
+}
+// object field can be a function also 
+// in above People interface greet is a function returning a string 
+
+let people: People = {
+    name: "karm",
+    age: 34, 
+    greet: () => {
+        return "hi"
+    }
+} 
+
+let greeting = people.greet?.()
+console.log(greeting) 
+
+
+
+class Manager implements People {
+    name: string;
+    age: number;
+    constructor(name: string , age: number){
+        this.name = name;
+        this.age = age;     
+    }
+}
