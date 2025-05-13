9c5b70e53d0bc4afb5c8a42d6277ef0d244cd2eb
Karmpatel18 <karmpatel1203@gmail.com>
2025-05-13 23:12:54 +0530

    Partial , readonly

--- web-dev-week-14/src/lecture_3.ts
+++ web-dev-week-14/src/lecture_3.ts
@@ -19,15 +19,33 @@ const result = addName({name:"karm",age:34},{name:"patel",age:12});
 
 interface Client {
     name: string,
-    id: string,
+    readonly id: string,
     age: number, 
     password: string,
 }
+// when readonly is assigned to any field it can't be changed 
+// example
+
+const client: Client = {
+    name: "karm patel",
+    id: "324223524242",
+    age: 498,
+    password: "askldfajf"
+}
+
+// client.id = "454545445"   <<  here typescript will complain because readonly does not let anyone to change the internal field value  
 
 type updatedProps = Pick<Client , 'age' | 'id' | 'password'>
 
-function updateClient(client: updatedProps){
-    const result = console.log(client.age + client.id + client.password);
+// use of Partial 
+// can be used to partially when you want to partially choose in the interface or a type.  
+
+type partialProps = Partial<updatedProps>
+
+function updateClient(client: partialProps){
+    const result = console.log(
+        (client.age ?? 0) + (client.id ?? '') + (client.password ?? '')
+    );
     return result; 
 }
 
